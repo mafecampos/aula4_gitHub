687c43e150b98ba4ec8ddbbc2e3e8ecf090cf6af
Maria Fernanda <131400153+mafecampos@users.noreply.github.com>
2024-03-06 12:27:49 -0300

    branch_test
    Código teste de branch.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,24 @@ class HelloWorld
 {
     static void Main(String[] args)
     {
-        string nome = "Mafe";
+        string nome = "Mafe amiga da Livs";
         Console.WriteLine(nome);
+        Console.WriteLine("Sabia que eu tenho poderes? Escolhe um número de 1 a 3 ai");
+        int escolha = Convert.ToInt32(Console.ReadLine());
+        switch (escolha)
+        {
+            case 1:
+                Console.BackgroundColor = ConsoleColor.Black;
+                break;
+            case 2:
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                break;
+            case 3:
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                break;
+        }
+        Console.Clear();
+        Console.WriteLine("EU SOU O SUPER PINTADINHO");
+        Console.ReadLine();
     }
 }
