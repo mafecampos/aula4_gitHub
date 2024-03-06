dc49462477c50297b73910f55e2aeb7429d8aecc
Maria Fernanda <131400153+mafecampos@users.noreply.github.com>
2024-03-06 12:40:49 -0300

    Develop_Pintadinho
    Aqui a duplinha da Livia, no caso, eu Mafe, adicionei um alterador de background.

--- Program.cs
+++ Program.cs
@@ -7,5 +7,22 @@ class HelloWorld
         Console.WriteLine(nome);
         string nome_colega = "Livs bestie";
         Console.WriteLine("\n"nome_colega);
+        Console.WriteLine("Sabia que eu tenho poderes? Escolhe um número de 1 a 3 ai");
+        int escolha = Convert.ToInt32(Console.ReadLine());
+        switch (escolha)
+        {
+            case 1:
+                Console.BackgroundColor = ConsoleColor.Black;
+                break;
+            case 2:
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                break;
+            case 3:
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                break;
+        }
+        Console.Clear();
+        Console.WriteLine("EU SOU O SUPER PINTADINHO");
+        Console.ReadLine();
     }
 }
